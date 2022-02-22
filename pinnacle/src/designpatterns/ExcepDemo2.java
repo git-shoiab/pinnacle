b3bc5b794fddc7cfb10e02e93f96588b16786d2e
hit300b1 <hit300b1@gmail.com>
2022-02-22 17:20:00 +0530

    22-feb

---
+++ pinnacle/src/designpatterns/ExcepDemo2.java
@@ -0,0 +1,25 @@
+package designpatterns;
+//https://fluvid.com/videos/detail/5Ad-5C1_dGiVGO1QE#.YhTC18fa3tw.link
+public class ExcepDemo2 {
+	public static void main(String[] args) {//throws Exception {
+		ExcepDemo2 obj=new ExcepDemo2();
+		try {
+			obj.met2(10);
+		}catch(Exception e) {
+			
+		}
+	}
+	void met(){
+		try {
+			wait();
+		}catch(Exception e) {
+			
+		}
+	}
+	
+	void met2(int i)throws Exception {
+		if(i==0) {
+			throw new Exception();
+		}
+	}
+}
---
+++ pinnacle/src/designpatterns/VisitorPattern.java
@@ -0,0 +1,77 @@
+package designpatterns;
+//https://fluvid.com/videos/detail/ykZL6ck9jLiY9w2zo#.YhTNuPmWDig.link
+import java.util.Scanner;
+
+public class VisitorPattern {
+	public static void main(String[] args) {
+		
+		Child baby=new Child();
+		System.out.println("please input and enter to proceed....");
+		Scanner scan=new Scanner(System.in);
+		
+		String n=scan.next();
+		
+		Dog tiger=new Dog();
+		
+		
+		baby.playWithDog(tiger, n);
+	}
+}
+
+class Dog{
+	public void play(String item)throws DogExceptions {
+		if(item.equals("stick")) {
+			throw new DogBiteException();
+		}
+		else if(item.equals("stone")) {
+			throw new DogBarkException();
+		}
+		else if(item.equals("bone")) {
+			throw new DogHappyException();
+		}
+	}
+}
+
+abstract class DogExceptions extends Exception{
+	public abstract void visit();
+}
+class DogBiteException extends DogExceptions{
+	@Override
+	public void visit() {
+		new Handler911().handle(this);
+	}
+}
+class DogBarkException extends DogExceptions{
+	@Override
+	public void visit() {
+		new Handler911().handle(this);
+	}
+}
+class DogHappyException extends DogExceptions{
+	@Override
+	public void visit() {
+		new Handler911().handle(this);
+	}
+}
+class Child{
+	void playWithDog(Dog dog,String item) {
+		try {
+			dog.play(item);
+		}catch(DogExceptions de){
+			de.printStackTrace();
+			de.visit();
+		}
+	}
+}
+
+class Handler911{
+	public void handle(DogBiteException dbe) {
+		System.out.println("dog has bitten, wait for the ambulance,,,,,,,,,,");
+	}
+	public void handle(DogBarkException dbr) {
+		System.out.println("no worries  just ignore...................");
+	}
+	public void handle(DogHappyException dbr) {
+		System.out.println("enjoy...................");
+	}
+}
