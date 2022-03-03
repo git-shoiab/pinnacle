7f2011fb02720a63f62160d0b407724d78d40094
hit300b1 <hit300b1@gmail.com>
2022-03-03 16:52:27 +0530

    3-mar

---
+++ pinnacle/src/threadpack/ThreadDemo8.java
@@ -0,0 +1,30 @@
+package threadpack;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ThreadDemo8 {
+	public static void main(String[] args)throws Exception {
+		ExecutorService es=Executors.newFixedThreadPool(1);
+		
+		Future obj= es.submit(new ThreadJob());
+		
+		Object value=obj.get();
+		
+		
+		System.out.println(value);
+		
+		es.shutdown();
+	}
+}
+
+class ThreadJob implements Callable{
+	@Override
+	public Object call() throws Exception {
+		// TODO Auto-generated method stub
+		return "returned by the call method of thread job....";
+	}
+}
+
